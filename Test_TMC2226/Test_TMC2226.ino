638a54ee0eda7a26ebbf821e0ba1a506d7f3838c
Akutagawa50 <ryusuke0525a@gmail.com>
2022-07-03 21:05:24 +0900

    回転方法を改善

--- Test_TMC2226/Test_TMC2226.ino
+++ Test_TMC2226/Test_TMC2226.ino
@@ -2,15 +2,17 @@ const int DIR  = 17;
 const int STEP = 16;
 const int EN = 18;
 const int pwmch = 0;
-bool dir_state;
+const int LED = 19;
+uint8_t counter = 0;
 
 void setup() {
   Serial.begin(115200);
-  dir_state = false;
   pinMode(DIR, OUTPUT);
   pinMode(EN, OUTPUT);
+  pinMode(LED, OUTPUT);
   digitalWrite(DIR, LOW);
   digitalWrite(EN, LOW);
+  digitalWrite(LED, LOW);
   
   ledcSetup(pwmch, 1, 8);
   ledcAttachPin(STEP, pwmch);
@@ -18,13 +20,20 @@ void setup() {
 }
 
 void loop() {
-  dir_state = !dir_state;
-  digitalWrite(DIR, dir_state);
-  
-  for (int i = 500; i < 3000; i += 500) {
+  digitalWrite(DIR, counter & 0x02);
+  digitalWrite(LED, counter & 0x01);
+  digitalWrite(EN, counter & 0x01);
+  ledcSetup(pwmch, 800, 8);
+  ledcWrite(pwmch, 16);
+  Serial.println(counter);
+  counter++;
+  delay(2000);
+  /*
+  for (int i = 500; i < 6000; i += 500) {
     ledcSetup(pwmch, i, 8);
     ledcWrite(pwmch, 128);
     Serial.println(i);
     delay(500);
   }
+  */
 }
